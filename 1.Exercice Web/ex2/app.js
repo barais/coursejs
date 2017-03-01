6904affbb615ec6a6a35e682e4e61067e33e6d35
Olivier Barais <barais@irisa.fr>
2017-03-01 15:41:45 +0100

    ne

--- 1.Exercice Web/ex2/app.js
+++ 1.Exercice Web/ex2/app.js
@@ -1,4 +1,23 @@
-function blinker()
+function goRed()
 {
-	
+			var d = document.getElementById("blink") ;
+			d.style.color='red';
+			d.innerHTML = 'La vie est belle';
+			setTimeout('goWhite()', 500);
+			var monimage = document.getElementById("img1") ;
+			monimage.src = 'img/img1.jpg';
+}
+function goWhite()
+{
+		var d = document.getElementById("blink") ;
+		d.style.color='blue';
+		d.innerHTML = 'La vie est moche';
+
+		var monimage = document.getElementById("img1") ;
+		monimage.src = 'img/img2.jpg';
+        setTimeout('goRed()', 1000);
+
+}
+window.onload = function(){
+	goRed();
 }
