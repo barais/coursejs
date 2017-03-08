591901814d68b4171ca7c34cc8fc789fe7729973
Olivier Barais <barais@irisa.fr>
2017-03-08 10:36:03 +0100

    new

--- 2.Exercice NodeJs/5.Projet NodeJS sur Raspberry Pi/client/client.js
+++ 2.Exercice NodeJs/5.Projet NodeJS sur Raspberry Pi/client/client.js
@@ -1,21 +1,109 @@
 $(function(){
 
-  var meteo = true;
   $("#encode").click(function(){
-//    console.log($("#montext").val());
   var data = new Object();
-  data.encode = false;
   data.morse= $("#montext").val();
   $.post('/morse',data,function(result){
-      meteo = result;
-      if (meteo == 'fait beau'){
-
-      }
-      
-  });
-
-
-
+    console.log(result.titi);
+    clignotte(result.titi,0)
 
   });
 });
+
+function clignotte(machaine, i){
+
+  var c = machaine.substring(i,i+1);
+  if (c== '.'){
+    swithon(machaine, i,1000);
+  }else if(c=="-"){
+    swithon(machaine, i,2000);
+  }else{
+    $("#malumiere").attr("src", 'img/led.svg');
+    setTimeout(function(){
+      waitEteint(machaine,i);
+    },1000);
+  }
+}
+
+
+function waitEteint(machaine,i){
+  setTimeout(function(){
+    i = i+1;
+    if (i< machaine.length){
+    clignotte(machaine,i);
+  }
+},1000);
+}
+
+function swithon(machaine,i,time){
+  $("#malumiere").attr("src", 'img/led-allume.svg');
+  setTimeout(function(){
+    $("#malumiere").attr("src", 'img/led.svg');
+    waitEteint(machaine,i);
+  },time);
+}
+
+
+
+  $("#ellume").click(function(){
+		    $("#malumiere").attr("src", 'img/led-allume.svg');
+  });
+  $("#eteindre").click(function(){
+		    $("#malumiere").attr("src", 'img/led.svg');
+  });
+
+
+});
+
+/*for(var i = 0; i< result.titi.length; i++){
+  var c = result.titi.substring(i,i+1);
+  if (c== '.'){
+    $("#malumiere").attr("src", 'img/led-allume.svg');
+    setTimeout(function(){
+      $("#malumiere").attr("src", 'img/led.svg');
+    },250);
+  }else if(c=="-"){
+    $("#malumiere").attr("src", 'img/led-allume.svg');
+    setTimeout(function(){
+      $("#malumiere").attr("src", 'img/led.svg');
+    },500);
+
+  }else{
+    $("#malumiere").attr("src", 'img/led.svg');
+    setTimeout(function(){
+    },500);
+
+  }*/
+
+
+  /*var i = 0;
+  var k= 0;
+  var j =0;
+  setInterval(function(){
+      if (i< result.titi.length){
+      if (j==1){
+        $("#malumiere").attr("src", 'img/led.svg');
+        j=0;
+      }else{
+      var c = result.titi.substring(i,i+1);
+      if (c== '.'){
+        $("#malumiere").attr("src", 'img/led-allume.svg');
+        i = i+1;
+        j= j+1;
+      }else if(c=="-"){
+        $("#malumiere").attr("src", 'img/led-allume.svg');
+        if (k == 1){
+          i = i+1;
+          k=0;
+          j= j+1;
+        }else {
+          k = k+1;
+        }
+      }
+      else{
+        $("#malumiere").attr("src", 'img/led.svg');
+        i = i+1;
+        j= j+1;
+      }
+    }}
+  },1000);*/
--- 2.Exercice NodeJs/5.Projet NodeJS sur Raspberry Pi/server/serverMorse.js
+++ 2.Exercice NodeJs/5.Projet NodeJS sur Raspberry Pi/server/serverMorse.js
@@ -7,26 +7,15 @@ var morse = require('morse');
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
    extended: true
-})); 
- 
+}));
+
 
 
 app.post('/morse', function (req, res) {
-	if (req.body.encode == 'true'){
 		var encoded = morse.encode(req.body.morse);
 		var result = new Object();
 		result.titi = encoded;
 		res.send(result);
-	}else {
-		var result = new Object();
-		result.titi = req.body.morse;
-		res.send(result);
-	}
-
-
-});
-app.post('/morse2', function (req, res) {
-	console.log('hello world');
 });
 
 
